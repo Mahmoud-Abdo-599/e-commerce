f417ff6b9f0bc3042db4af832060b2d60de104d9
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-05-05 09:56:14 +0200

    create cart-item component and connect cart dropdown with cartitem

--- src/components/collection-preview/CollectionPreview.js
+++ src/components/collection-preview/CollectionPreview.js
@@ -8,8 +8,8 @@ const CollectionPreview = ({title, items}) =>  (
             <div className="preview">
                  {
                      items.filter((item, idx) => idx<4)
-                     .map(({id, ...otherTtemProps})=>(
-                         <CollectionItem key={id} {...otherTtemProps} />
+                     .map((item)=>(
+                         <CollectionItem key={item.id} item={item} />
                      ))
                  }
 
